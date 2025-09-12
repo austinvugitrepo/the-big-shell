7e7b8cb6bacfeda2f7710485d4e46a20b1d8f2ff
Austin <freeluck.v.i.p999@gmail.com>
2025-09-12 03:10:54 -0500

    made small chanes mainly to clean up code no new features.

--- shell.c
+++ shell.c
@@ -2,24 +2,34 @@
 #include <stdlib.h> //libaries 
 #include <string.h>
 
-int main(){   // start of the main function
+char input[1500];  // holds the characters for strings
+int reader(); //reads input exits shell when EOF 
+int exiter(); // if input = exit terminates shell
+
+int main(){ // start of the main function
   
-while(1){ //shell loop
+  while(1){
 
- char input[1500];  // holds the characters for strings
- printf("%s", "$ "); //prompt
- fflush(stdout);    //ensures prompt gets shown right away
-
-  if (fgets(input,sizeof(input), stdin) == NULL){
-        break;                                    //reads input
-  }
-
-
-  input[strcspn(input, "\n")] = '\0'; //removes \n 
-  
-  if (strcmp(input, "exit") == 0){       //if exit closes program
+   printf("%s", "$ "); //prompt
+   fflush(stdout);    //ensures prompt gets shown right away
+   if(!reader()){
       break;
     }
+   input[strcspn(input, "\n")] = '\0'; //removes \n 
+   if(!exiter()){
+      break;
+    }  
 }
- return 0;
+}
+  int reader(){
+
+   if (fgets(input,sizeof(input), stdin) == NULL){
+        return 0;                                  
+  }
+ }
+  int exiter(){
+   
+    if (strcmp(input, "exit") == 0){
+       return 0;
+}
 }
